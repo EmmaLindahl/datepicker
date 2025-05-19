5ebe793b9750bdaf33051ee4cc4858b09cd4f6fa
Pontus lindh <Pontus.lindh.92@gmail.com>
2025-05-19 22:41:11 +0200

    Remove Tailwind CSS warning from DatePicker component

--- src/DatePicker/index.tsx
+++ src/DatePicker/index.tsx
@@ -40,10 +40,6 @@ export const DatePicker = ({
   }, [open]);
 
   useEffect(() => {
-      if (!window.getComputedStyle(document.documentElement).getPropertyValue('--tw-bg-opacity')) {
-    console.warn("Tailwind CSS is not detected. Please ensure Tailwind is set up in your project.");
-  }
-
     const handleClickOutside = (event: MouseEvent) => {
       if (
         wrapperRef.current &&
